eb76c570bb4f797ed52bb986ddb6934339d7cb3c
Mina Abskhron <minaabskhron@gmail.com>
2025-04-12 20:40:34 +0200

    done for signup

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -3,7 +3,59 @@
 import signupImg from "@/public/signup.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { baseUrl } from "../_lib/const";
+
 const page = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRepassword] = useState("");
+  const [errors, setErrors] = useState("");
+  const router = useRouter();
+
+  const isDisabled =
+    name.trim() === "" ||
+    email.trim() === "" ||
+    phone.trim() === "" ||
+    password.trim() === "" ||
+    rePassword.trim() === "";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== rePassword) {
+      setErrors("Passwords don't match");
+      return;
+    }
+    try {
+      const res = await fetch(`${baseUrl}/api/v1/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password, phone, rePassword }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+      if (!res.ok) {
+        throw new Error(data.errors?.msg || data.message || "Sign up failed");
+      }
+      const signInRes = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+      if (signInRes?.ok) {
+        router.push("/");
+      } else {
+        router.push("/signin"); // Redirect to sign-in if auto-login fails
+      }
+    } catch (error) {
+      setErrors(error.message);
+    }
+  };
   return (
     <div className="flex justify-evenly items-center mt-10  ">
       <div>
@@ -14,7 +66,7 @@ const page = () => {
           Get Start Shopping
         </h1>
         <p>Welcome back to FreshCart! Enter your email to get started.</p>
-        <form action="" className="mt-10">
+        <form onSubmit={handleSubmit} className="mt-10">
           <label htmlFor="name" className="block pb-2 font-semibold text-sm">
             Name
           </label>
@@ -24,6 +76,8 @@ const page = () => {
             type="name"
             name="name"
             required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="ps-2 w-full py-2 border-2 border-gray-300 rounded-lg"
           />
           <label htmlFor="email" className="block my-2 font-semibold text-sm">
@@ -34,6 +88,8 @@ const page = () => {
             placeholder="Email"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
             className="ps-2 w-full py-2 border-2 border-gray-300 rounded-lg"
           />
@@ -46,6 +102,8 @@ const page = () => {
             placeholder="Phone"
             type="phone"
             name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
             required
             className="ps-2 w-full py-2 border-2 border-gray-300 rounded-lg"
           />
@@ -60,25 +118,32 @@ const page = () => {
             placeholder="********"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
             className="ps-2 w-full py-2 border-2 border-gray-300 rounded-lg"
           />
           <label
-            htmlFor="repassword"
+            htmlFor="rePassword"
             className="block my-2 font-semibold text-sm"
           >
             rePassword
           </label>
           <input
-            id="repassword"
+            id="rePassword"
             placeholder="********"
-            type="repassword"
-            name="repassword"
+            type="password"
+            name="rePassword"
+            value={rePassword}
+            onChange={(e) => setRepassword(e.target.value)}
             required
             className="ps-2 w-full py-2 border-2 border-gray-300 rounded-lg"
           />
+          {errors && (
+            <p className="bg-red-200 p-2 px-2 mt-4 rounded-xl">{errors}</p>
+          )}
           <button
-            disabled
+            disabled={isDisabled}
             className="w-full mt-10 disabled:opacity-75 disabled:cursor-not-allowed bg-green-700 rounded-lg py-2 text-white cursor-pointer hover:bg-green-800"
           >
             Sign up
@@ -86,7 +151,7 @@ const page = () => {
         </form>
         <div className="font-semibold mt-5 mb-20">
           <p className="mb-2">
-            You have an account?{" "}
+            You have an account?
             <Link className="text-green-700" href="/signin">
               Sign In
             </Link>
