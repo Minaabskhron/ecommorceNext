0ce5d11c053d867166d50d539dd26689140ad028
minaabskhron <minaabskhron@gmail.com>
2025-04-21 17:57:39 +0200

    env changes

--- app/_lib/const.js
+++ app/_lib/const.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://ecommerce.routemisr.com";
+export const baseUrl = process.env.BASE_URL;
 export const getTopSellingProducts = (products) => {
   return products.sort((a, b) => b.sold - a.sold).slice(0, 20);
 };
