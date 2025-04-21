8899be86cdfba4b04d5bf3507e2e4c7a6ee68c77
minaabskhron <minaabskhron@gmail.com>
2025-04-21 21:32:02 +0200

    footer responsive

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -13,64 +13,66 @@ const Footer = () => {
     <footer className="bg-[#eee] pt-10 p-5 text-gray-500 absloute bottom-0 left-0 right-0">
       <h3 className="text-black text-2xl mb-2">Get The FreshCart App</h3>
       <p>We will send you a link, open it on your phone to download the app.</p>
-      <div className="flex gap-3 justify-center items-center border-b pb-6 border-b-gray-300">
+      <div className="sm:flex mt-5 gap-3 justify-center items-center border-b pb-6 border-b-gray-300">
         <input
           type="text"
           placeholder="Email..."
-          className="text-black flex-grow border border-gray-300 rounded-xl bg-white p-1 ps-3
+          className="text-black w-full mt-5 sm:mt-0 sm:w-fit flex-grow border border-gray-300 rounded-xl bg-white p-1 ps-3
          focus:outline-none "
         />
-        <button className="bg-green-600 text-white px-3 py-1 rounded-xl">
+        <button className="bg-green-600 w-full mt-5 sm:mt-0 sm:w-fit text-white px-3 py-1 rounded-xl">
           Share App Link
         </button>
       </div>
-      <div className="flex justify-between border-b pb-6 border-b-gray-300 pt-6">
-        <div className="sm:flex">
-          <p className="text-black me-2 font-semibold">Payment Partners</p>
-          <ul className="flex gap-2">
-            <li>
-              <Link href="#">
-                <Image src={amazon} alt="amazon" />
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Image src={americanExpress} alt="american express" />
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Image src={masterCard} alt="master card" />
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Image src={paypal} alt="paypal" />
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Image src={visa} alt="visa" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="flex items-center">
-          <p className="font-semibold text-black me-4">
-            Get deliveries with FreshCart
-          </p>
-          <ul className="flex gap-2">
-            <li>
-              <Link href="#">
-                <Image src={googleplay} alt="googleplay" />
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Image src={applestore} alt="applestore" />
-              </Link>
-            </li>
-          </ul>
+      <div className="flex justify-center lg:block">
+        <div className="lg:flex justify-between border-b pb-6 border-b-gray-300 pt-6">
+          <div className="flex mb-2 lg:mb-0">
+            <p className="text-black me-2 font-semibold">Payment Partners</p>
+            <ul className="flex gap-2">
+              <li>
+                <Link href="#">
+                  <Image src={amazon} alt="amazon" />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <Image src={americanExpress} alt="american express" />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <Image src={masterCard} alt="master card" />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <Image src={paypal} alt="paypal" />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <Image src={visa} alt="visa" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="lg:flex items-center">
+            <p className="font-semibold mb-2 lg:mb-0 text-black me-4 text-center">
+              Get deliveries with FreshCart
+            </p>
+            <ul className="flex gap-2">
+              <li>
+                <Link href="#">
+                  <Image src={googleplay} alt="googleplay" />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <Image src={applestore} alt="applestore" />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const page = async () => {
 
   return (
     <>
-      <h1 className="text-3xl font-semibold mb-10 text-gray-500">
+      <h1 className="text-3xl sm:ms-10 font-semibold mb-10 text-gray-500">
         Most selling products
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:mx-12 sm:gap-5 lg:grid-cols-4 gap-2">
